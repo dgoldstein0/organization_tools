de0630d685b95e117dbaff95d000ab6dbd56f4f3
David Goldstein <dgoldstein0@gmail.com>
2024-12-29 13:23:36 -0800

    convert find_duplicates to python

--- find_duplicates.ps1
+++
@@ -1,9 +0,0 @@
-Get-ChildItem -Recurse -File `
-| Group-Object -Property Length `
-| ?{ $_.Count -gt 1 } `
-| %{ $_.Group } `
-| Get-FileHash `
-| Group-Object -Property Hash `
-| ?{ $_.Count -gt 1 } `
-| %{ $_.Group } `
-| Format-list Hash, Path
---
+++ find_duplicates.py
@@ -0,0 +1,42 @@
+import argparse
+import hashlib
+
+from collections import defaultdict
+from pathlib import Path
+
+
+def find_duplicates(folder: Path):
+    paths_by_size = defaultdict(list)
+
+    for dirpath, dirnames, filenames in folder.walk():
+        for fname in filenames:
+            fpath = dirpath / fname
+            paths_by_size[fpath.stat().st_size].append(fpath)
+
+
+    paths_by_hash = defaultdict(list)
+    for paths in paths_by_size.values():
+        if len(paths) > 1:
+            for fpath in paths:
+                m = hashlib.sha256()
+                with open(fpath, "rb") as f:
+                    m.update(f.read())
+                paths_by_hash[m.hexdigest()].append(fpath)
+
+    for paths in paths_by_hash.values():
+        if len(paths) > 1:
+            print("Found duplicate files")
+            for path in paths:
+                print("- ", path)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--folder", type=Path)
+    args = parser.parse_args()
+
+    find_duplicates(args.folder)
+
+
+if __name__ == "__main__":
+    main()
